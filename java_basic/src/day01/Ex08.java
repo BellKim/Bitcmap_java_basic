ab9b5178e2fc499a11f1dcab15aa7c38a92becf0
bit_jonghyun <goste390@gmail.com>
2019-11-04 18:23:01 +0900

    add Ex08.java

--- java_basic/src/day01/Ex08.java
+++ java_basic/src/day01/Ex08.java
@@ -1,5 +1,8 @@
 package day01;
 //별찍기.
+//chojeyungbit@gmail.com
+//chojeyungbit@gmail.com
+
 public class Ex08 {
 
 	public static void main(String[] args) {
@@ -11,17 +14,34 @@ public class Ex08 {
 			}
 			System.out.println();
 		}//end for
-		
-								System.out.println();
+//===============================================================
+		System.out.println();
 		System.out.println("2번");
-		for(int i = 1; i<=5; i++) {
-			for(int j = 6; j>i; j--) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 6; j > i; j--) {
 				System.out.print("*");
 			}
 			System.out.println();
-		}//end for
-		
-								System.out.println();
+		} // end for
+//===============================================================	
+		System.out.println();
+		System.out.println("2번 변형");
+		for (int i = 1; i <= 5; i++) {
+			String starPrint = " ";
+			for (int j = i; j<=5; j++) {
+//				String을 연결하는 방법1. 
+//				+연산자를 이용한다.
+//				StartPrint = starPrint + "*";
+//				위의 코드를 간략화 시키면
+				starPrint += "*";
+				
+			}
+			System.out.println(starPrint);
+		} // end for
+
+
+//===============================================================
+		System.out.println();
 		System.out.println("3번");
 		for(int i = 1; i<=5; i++) {
 			for(int j = 5; j>i; j--) {
@@ -33,7 +53,31 @@ public class Ex08 {
 			}
 			System.out.println("");
 		}//end for
-		
+//==================================================
+		System.out.println("3번 변형");
+		for(int i = 1; i<=5; i++) {
+			String starPrint = "";
+			for (int j = 0; j < 5-i; j++) {
+				starPrint = starPrint.concat("  ");
+			}
+			for(int j = 1; j<=i; j++) {
+				starPrint = starPrint.concat("*");
+			}
+			System.out.println(starPrint);
+		}//end for
+//===============================================================		
+		System.out.println("3번 연결");
+		for(int i = 1; i<=5; i++) {
+			StringBuilder stringBuilder = new StringBuilder();
+			for (int j = 0; j < i-1; j++) {
+				stringBuilder.append(' ');//작은따옴표 여러개 넣으면 오류. 
+			}
+			for(int j = i; j<= 5; j++) {
+				stringBuilder.append("*");
+			}
+			System.out.println	(stringBuilder.toString());
+		}//end for
+//===============================================================		
 		System.out.println();
 		
 		System.out.println("4번");
@@ -47,7 +91,7 @@ public class Ex08 {
 			}
 			System.out.println();
 		} // end for
-		
+//===============================================================		
 		System.out.print("5번");
 		for (int i = 0; i <= 5; i++) {
 			for (int j = 6; j > i; j--) {
@@ -61,7 +105,7 @@ public class Ex08 {
 		} // end for
 		
 		System.out.println();
-		
+//===============================================================		
 		System.out.println("6번");
 		for (int i = 0; i <= 5; i++) {
 			
@@ -73,10 +117,35 @@ public class Ex08 {
 				System.out.print("*");
 			}
 			System.out.println();
-			
-			
 		} // end for
-		
+//===============================================================		
+		System.out.println("7번");
+		for (int i = 1; i <=10; i++) {
+			if(i<=5) {
+				for (int j = 0; j < i; j++) {
+					System.out.print("*");
+				}
+			}else {
+				for (int j = 10; j > i; j--) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		} // end for
+//===============================================================		
+		System.out.println("8번");
+		for (int i = 1; i <=10; i++) {
+			if(i<=5) {
+				for (int j = 0; j < i; j++) {
+					System.out.print("*");
+				}
+			}else {
+				for (int j = 10; j > i; j--) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		} // end for
 		
 		
 		
