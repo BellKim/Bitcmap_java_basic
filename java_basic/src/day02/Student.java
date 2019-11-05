35d9a901b2d2241e0a4718407c27e2ae3de48773
bit_jonghyun <goste390@gmail.com>
2019-11-05 12:19:59 +0900

    getter, setter setting. 캡슐화

--- java_basic/src/day02/Student.java
+++ java_basic/src/day02/Student.java
@@ -18,48 +18,61 @@ public class Student {
 	
 	
 	public String name;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-		//필드의 name이다. 파라미터로 넘어온 name
-	}
 	public int age;
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public int getKor() {
-		return kor;
-	}
-	public void setKor(int kor) {
-		this.kor = kor;
-	}
-	public int getEng() {
-		return eng;
-	}
-	public void setEng(int eng) {
-		this.eng = eng;
-	}
-	public int getMath() {
-		return math;
-	}
-	public void setMath(int math) {
-		this.math = math;
-	}
-	public String getJumin() {
-		return jumin;
-	}
-	public void setJumin(String jumin) {
-		this.jumin = jumin;
-	}
 	public int kor;
 	public int eng;
 	public int math;
 	public String jumin;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public String getJumin() {
+		return jumin;
+	}
+
+	public void setJumin(String jumin) {
+		this.jumin = jumin;
+	}
+
+
 	public void study() {
 		System.out.println("공부합시다!!");
 	}//study.
