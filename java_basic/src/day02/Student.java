5f3d41f2dbb74be50b7f57d7f3852fff6af85124
bit_jonghyun <goste390@gmail.com>
2019-11-05 12:10:45 +0900

    접근제한자에 관한설명 및 캡슐화 getter, setter
    //	접근제한자
    //	접근가능한 범위를 지정한다.
    //	public		:	어디서든지 접근가능(같은프로젝트 내부에서 모두 접근가능)
    //	protected	:	같은패키지 + 상속받는 클래스까지 접근가능.
    //	package		:	같은패키지에서만 접근가능(아무것도 안적어주면package이다.)
    //		=> default = package 와 같은 레벨.
    //	private		:	같은 클래스에서만 접근가능.
    	//접근제한자가 아닌것은?

--- java_basic/src/day02/Student.java
+++ java_basic/src/day02/Student.java
@@ -16,8 +16,46 @@ public class Student {
 	
 //	캡슐화 : 필드 하나하나를 캡슐화 해서 외부에서의 개별접근을 막는것. 외부클래스를 통해서 값을 넣고 빼는것을 하는것 
 	
+	
 	public String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+		//필드의 name이다. 파라미터로 넘어온 name
+	}
 	public int age;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	public String getJumin() {
+		return jumin;
+	}
+	public void setJumin(String jumin) {
+		this.jumin = jumin;
+	}
 	public int kor;
 	public int eng;
 	public int math;
