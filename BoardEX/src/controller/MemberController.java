ae2705ac84e8ef10a00e3a2922617e061a5eaa13
bit_jonghyun <goste390@gmail.com>
2019-11-11 11:27:28 +0900

    board example02
    member join code add.

--- BoardEX/src/controller/MemberController.java
+++ BoardEX/src/controller/MemberController.java
@@ -6,20 +6,21 @@ import dto.MemberDTO;
 
 public class MemberController {
 	ArrayList<MemberDTO> list;
-	public void MemerController() {
+
+	public MemberController() {//void 붙히면 안된다.
 		list = new ArrayList<MemberDTO>();
 		MemberDTO m1 = new MemberDTO();
 		m1.setId(1);
 		m1.setUserId("admin");
 		m1.setPassword("111");
 		m1.setName("관리지");
-		
+
 		MemberDTO m2 = new MemberDTO();
 		m2.setId(1);
 		m2.setUserId("admin");
 		m2.setPassword("111");
 		m2.setName("관리지");
-		
+
 		MemberDTO m3 = new MemberDTO();
 		m3.setId(1);
 		m3.setUserId("admin");
@@ -30,20 +31,35 @@ public class MemberController {
 		list.add(m2);
 		list.add(m3);
 
-	}//MemberController()
-	
+	}// MemberController()
+
 	public MemberDTO selectOne(String userId, String password) {
 		for (MemberDTO m : list) {
 			if (m.getUserId().equals(userId) && m.getPassword().equals(password)) {
-				//입력받은 비번과 아이디가 저장된 아이디비번과 같을때
+				// 입력받은 비번과 아이디가 저장된 아이디비번과 같을때
 				return m;
 
 			}
 		}
-		
-		
-		
+
 		return null;
+	}
+	
+	//중복확인하기.
+	public void insert (MemberDTO memberDTO) {
+		boolean isAlready = false;
+		for(MemberDTO m : list) {
+			if(m.getUserId().equals(memberDTO.getUserId())) {
+				isAlready = true;
+			}//이미 같은 아이디가 있으면, insert 자체를 실행안하도록 한다.
+		}
+		if(!isAlready) {
+			int id = list.get(list.size()-1).getId() +1;
+			memberDTO.setId(id);
+			list.add(memberDTO);
+		}else {
+			System.out.println("이미 존재하는 아이디 입니다. ");
+		}
 	}
 	
 
--- BoardEX/src/viewer/BoardViewer.java
+++ BoardEX/src/viewer/BoardViewer.java
@@ -10,46 +10,67 @@ public class BoardViewer {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		MemberController memberController = new MemberController();
-		
-		while(true) {
-			//기본화면에서 로그인할지 종료할지 선택 -> 종료면 종료, 로그인이면 로그인 화면으로 이동한다.
+
+		while (true) {
+			// 기본화면에서 로그인할지 종료할지 선택 -> 종료면 종료, 로그인이면 로그인 화면으로 이동한다.
 			System.out.println("========= 게시판 입니다. ===========");
 			System.out.println("1. 로그인, 2. 종료");
 			int choice = scan.nextInt();
-			if(choice == 1) {
-				MemberDTO logInUser = logIn(scan, memberController);
-				//로그인 메소드 호출
-				if((logInUser != null) == true) {
-					System.out.println("로그인 성공\n admin사용자님 환영합니다.");
-				}else {
-					System.out.println("로그인 실패 !!!");
+			if (choice == 1) {
+				System.out.println("1. 로그인,,,,,    2. 회원가입");
+				choice = scan.nextInt();
+				if(choice == 1) {
+					// 로그인 메소드 호출
+					MemberDTO logInUser = logIn(scan, memberController);
+					if ((logInUser != null)) {
+						System.out.println("로그인 성공\n"+ logInUser.getName()  +" 사용자님 환영합니다.");
+						//로그인 성공 이후 코드를 여기에
+						System.out.println("1. 게시판보기, 2. 회원정보보기");
+						choice = scan.nextInt();
+						if(choice == 1) {
+							//게시판 목록보는 코드부터 들어간다.
+						}else if(choice ==2) {
+							//회원 정보를 출력하고 수정할 수 있는 코드가 들어간다.
+						}
+						
+					} else {
+						System.out.println("로그인 실패 !!!");
+					}					
+				}else if(choice == 2) {
+					//회원가입메소드 실행
+					scan.nextLine();
+					MemberDTO memberDTO = new MemberDTO();
+					System.out.print("사용자 id: ");
+					memberDTO.setUserId(scan.nextLine());
+					System.out.print("사용자 pw: ");
+					memberDTO.setPassword(scan.nextLine());
+					System.out.print("사용자 닉네임: ");
+					memberDTO.setName(scan.nextLine());
+					memberController.insert(memberDTO);
 				}
-			}else if(choice == 2) {
+			} else if (choice == 2) {
 				System.out.println("이용해주셔섯 감사합니다. ");
 				break;
 			}
 		}
-		
-		
-		
-		
+
 		scan.close();
 
-	}//end of main
-	
-	private static boolean logIn(Scanner scan) {
+	}// end of main
+
+	private static MemberDTO logIn(Scanner scan, MemberController memberController) {
 		scan.nextLine();//
 		System.out.print("ID : ");
 		String userId = scan.nextLine();
 		System.out.print("Password : ");
 		String password = scan.nextLine();
-		if(userId.equals("admin")&&password.equals("111")) {
-			return true;
-		}else {
-			return false;
-		}
-		
-	}//logIn
-	
+//		if(userId.equals("admin")&&password.equals("111")) {
+//			return true;
+//		}else {
+//			return false;
+//		}
+		return memberController.selectOne(userId, password);
 
-}//end of class
+	}// logIn
+
+}// end of class
