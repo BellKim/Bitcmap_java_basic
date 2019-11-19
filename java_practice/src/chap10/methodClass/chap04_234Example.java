bfc500046cacd37536b669dfeb881db36895c838
JongHyun <goste390@gmail.com>
2019-11-20 03:43:49 +0900

    팀스터티 내용 추가.

---
+++ java_practice/src/chap10/methodClass/chap04_234Example.java
@@ -0,0 +1,25 @@
+package chap10.methodClass;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+
+public class chap04_234Example {
+	 static String date;
+
+	public static void main(String[] args) {
+		LocalDate nDate = LocalDate.now();
+		date = nDate.toString();
+		System.out.println(date);
+		
+		
+		System.out.println("오늘날자 \n"+Calendar.getInstance());
+		
+		
+		static Calendar nowDate1 = Calendar.getInstance();
+		
+		
+		
+
+	}
+
+}
