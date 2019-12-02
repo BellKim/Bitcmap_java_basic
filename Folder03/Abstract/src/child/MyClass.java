17bbc1a988dfaeff7f479a08202d66221e5320d3
bit_jonghyun <goste390@gmail.com>
2019-12-02 20:50:40 +0900

    abstract in class

---
+++ Folder03/Abstract/src/child/MyClass.java
@@ -0,0 +1,15 @@
+package child;
+
+import abstractClass.AbstractClass;
+
+public class MyClass extends AbstractClass {
+
+	@Override
+	public void abstractMethod() {
+		System.out.println("Myclass abstractMethod() ;;;;  추상 메소드의 정의 ");
+		
+	}
+
+
+
+}//end of class
