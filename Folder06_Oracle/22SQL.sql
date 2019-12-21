07b66b95a4652e5f1cbefab598c7ea451c12f632
Bellkim <goste390@gmail.com>
2019-12-21 20:39:04 +0900

    change file name

---
+++ Folder06_Oracle/22SQL.sql
@@ -0,0 +1,22 @@
+
+명칭 개요 문장으로.
+의미
+예제코드
+
+
+
+left right 
+
+natural join(inner join 인데 column 명으로 자동적으로 뽑아준다. (WHERE 컬럼명 = ))
+
+rank dense_rank 
+
+
+개요
+
+rollup, 
+cube, 
+grouping set group by 내용을 토대로 union all 해줌. 
+
+
+
