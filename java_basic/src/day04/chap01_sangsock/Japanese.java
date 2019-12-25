94631a32e3b9e1f996182a79b661ffdb64acf8a9
JongHyun <goste390@gmail.com>
2019-12-25 22:42:30 +0900

    delete files

--- java_basic/src/day04/chap01_sangsock/Japanese.java
+++
@@ -1,7 +0,0 @@
-package day04.chap01_sangsock;
-
-public class Japanese implements Human {
-	public void speak() {
-		System.out.println("일본말을 합니다.");
-	}
-}
--- java_basic/src/day04/chap02_interface/ConnectionMaker.java
+++
@@ -1,7 +0,0 @@
-package day04.chap02_interface;
-
-import java.sql.Connection;
-
-public interface ConnectionMaker {
-	public Connection makeConnection();
-}//end of interface
