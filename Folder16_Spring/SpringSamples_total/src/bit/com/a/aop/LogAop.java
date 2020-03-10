c0e64af768ae2fff5f98b0626223cae4b6e0636c
bit_jonghyun <goste390@gmail.com>
2020-03-10 10:25:30 +0900

    error

--- Folder16_Spring/SpringSamples_total/src/bit/com/a/aop/LogAop.java
+++ Folder16_Spring/SpringSamples_total/src/bit/com/a/aop/LogAop.java
@@ -1,8 +1,15 @@
 package bit.com.a.aop;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import bit.com.a.model.MemberDto;
 
 
 @Aspect
@@ -14,15 +21,28 @@ public class LogAop {
 		String signatureStr = joinpoint.getSignature().toShortString();
 		//session check
 		//~~ 의 약자로서 관점지향 프로그래밍이다. 
-		// 여러 다수의 공통된 작업을 한군데에서 수행하는 방식을  AOP 라고 한다. 
+		// 여러 다수의 공통된 작업을 한군데에서 수행하는 방식을  AOP 라고 한다.
+		
+		
+		
+		// 세션체크를 할때에도 사용할 수 있다.(모든jsp 파일에 작성해서 세션체크를 해야하지만 여기서는 그냥 가능하다.)
+		//session check
+//		HttpServletRequest request
+//		  =((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+
+//		if(request != null) {
+//			HttpSession session = request.getSession();
+//			MemberDto login = (MemberDto) session.getAttribute("login");
+//			if(login == null) {
+//				return "redirect:/login.do";
+//			}
+//		}
+		
 		
 
 		Object obj = joinpoint.proceed(); //기능실행!
-
-			
+	
 		System.out.println("loggerAop : " + signatureStr + " 종 료// 메소드 호출  ");
-		
-		
 		
 		return obj;
 		
---
+++ Folder16_Spring/SpringSamples_total/src/bit/com/a/util/BbsArrow.java
@@ -0,0 +1,25 @@
+package bit.com.a.util;
+
+public class BbsArrow {
+	private int depth = 0;
+	private String arrow;
+	
+	
+	public void setDepth(int depth) {//들어옴
+		this.depth = depth;
+	}
+
+	// 댓글의 여백과 이미지를 추가하는 함수
+	public String getArrow(int depth){//나감
+		String rs = "<img src='./image/arrow.png' width='20px' height='20px'/>";
+		String nbsp = "&nbsp;&nbsp;&nbsp;&nbsp;";
+		
+		String ts = "";
+		for(int i = 0;i < depth; i++){
+			ts += nbsp;
+		}	
+		return depth==0?"":ts + rs;
+	}
+	
+
+}
